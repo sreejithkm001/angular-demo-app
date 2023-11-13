36fad60e8524788ded518b3ed4e2ec16277e558b
SREEJITH  K M <sreejithkm3010@gmail.com>
2023-11-13 18:07:27 +0530

    updated

--- angular-demo-app/src/app/app.component.ts
+++ angular-demo-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,29 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-demo-app';
+// export class AppComponent {
+//   title = 'angular-demo-app';
+// }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.example();
+  }
+  example() {
+    var variable1 = 'TCS';
+    let variable2 = 'ACCENTURE';
+    const variable3 = 'BEINEX';
+
+    if (2 > 1) {
+      var variable1 = 'COGNIZANT';
+      let variable2 = 'CAPGEMINI'; 
+      console.log('1', variable1);
+      console.log('2', variable2);
+      console.log('3', variable3);
+    }
+    console.log('4', variable1); //variable value ressigned
+    console.log('5', variable2); //let cannot perform ressign
+    console.log('6', variable3);
+  }
 }
